0134069419591f4509fb3cb36c82af953cf9b51a
appinha <amanda_pinha@hotmail.com>
2022-12-01 20:21:27 -0300

    [day_01] Some revisions

--- my_solutions/day_01/main.py
+++ my_solutions/day_01/main.py
@@ -8,17 +8,16 @@ class DayPuzzleSolver():
         self.delimiter = "\n\n"
 
     def solve_part_1(self, raw_input):
-        ordered_elves = self._get_input(raw_input)
-        return sum(ordered_elves[-1:])
+        ordered_total_calories = self._get_input(raw_input)
+        return ordered_total_calories[0]
 
     def solve_part_2(self, raw_input):
-        ordered_elves = self._get_input(raw_input)
-        return sum(ordered_elves[-3:])
+        ordered_total_calories = self._get_input(raw_input)
+        return sum(ordered_total_calories[:3])
 
     def _get_input(self, raw_input):
-        elves = []
+        total_calories = []
         for elf in raw_input:
-            calories = elf.split("\n")
-            total_calories = sum([int(cal) for cal in calories if cal])
-            elves.append(total_calories)
-        return sorted(elves)
+            calories = map(int, elf.split("\n"))
+            total_calories.append(sum(calories))
+        return sorted(total_calories, reverse=True)

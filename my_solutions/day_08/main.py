ab7eb467e7e2ceb87651edb0568ee8da0f26fcd1
appinha <amanda_pinha@hotmail.com>
2022-12-08 18:52:31 -0300

    [day_08] Improved performance

--- my_solutions/day_08/main.py
+++ my_solutions/day_08/main.py
@@ -2,46 +2,39 @@ import sys; sys.path.insert(0, '..')
 import aoc_lib as lib
 from pprint import pprint
 
-from aoc_lib import HashGrid, NumpyGrid
-
 
 class DayPuzzleSolver():
     def __init__(self):
         self.delimiter = ""
 
     def solve_part_1(self, raw_input):
-        trees = HashGrid(raw_input, int)
+        trees = lib.Grid(string=raw_input, type=int)
 
-        def is_visible_in_line(pos, line):
-            for lp in line:
-                if trees.map[pos] <= trees.map[lp]:
-                    return False
-            return True
+        def is_not_visible_in_line(pos, line):
+            return any(trees[pos] <= trees[lp] for lp in line)
 
         not_visible_trees = []
         for pos, lines in trees.orthogonal_adjacencies_by_pos.items():
             if any([not line for line in lines]):
                 continue
-            is_visible = any(is_visible_in_line(pos, line) for line in lines)
-            if not is_visible:
+            if all(is_not_visible_in_line(pos, line) for line in lines):
                 not_visible_trees.append(pos)
 
         return trees.len - len(not_visible_trees)
 
     def solve_part_2(self, raw_input):
-        trees = HashGrid(raw_input, int)
+        trees = lib.Grid(string=raw_input, type=int)
+
         highest_score = 0
         for pos, lines in trees.orthogonal_adjacencies_by_pos.items():
-            scores = []
+            scores = 1
             for line in lines:
                 score = 0
                 for lp in line:
                     score += 1
-                    if trees.map[pos] <= trees.map[lp]:
+                    if trees[pos] <= trees[lp]:
                         break
-                scores.append(score)
-            if lib.prod(scores) > highest_score:
-                highest_score = lib.prod(scores)
+                scores *= score
+            if scores > highest_score:
+                highest_score = scores
         return highest_score
-
-

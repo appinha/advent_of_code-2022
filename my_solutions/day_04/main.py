083e76e3cd601d81467c680ca4cfba5645e3c50b
appinha <amanda_pinha@hotmail.com>
2022-12-25 18:57:03 -0300

    [day_04] Some minor revisions

--- my_solutions/day_04/main.py
+++ my_solutions/day_04/main.py
@@ -10,25 +10,28 @@ class DayPuzzleSolver():
     def solve_part_1(self, raw_input):
         result = 0
         for pair in raw_input:
-            ranges = self._get_ranges(pair)
-            if self._is_contained(*ranges) or self._is_contained(*reversed(ranges)):
+            ranges = process_input(pair)
+            if is_contained(*ranges) or is_contained(*reversed(ranges)):
                 result += 1
         return result
 
     def solve_part_2(self, raw_input):
         result = 0
         for pair in raw_input:
-            ranges = self._get_ranges(pair)
-            if self._has_overlap(*ranges):
+            ranges = process_input(pair)
+            if has_overlap(*ranges):
                 result += 1
         return result
 
-    def _get_ranges(self, pair):
-        return [list(map(int, r.split("-"))) for r in pair.split(",")]
 
-    def _is_contained(self, r1, r2):
-        return r1[0] >= r2[0] and r1[1] <= r2[1]
+def process_input(pair):
+    return [list(map(int, r.split("-"))) for r in pair.split(",")]
 
-    def _has_overlap(self, r1, r2):
-        r1, r2 = map(set, map(lambda r: range(r[0], r[1] + 1), [r1, r2]))
-        return r1.intersection(r2)
+
+def is_contained(r1, r2):
+    return r1[0] >= r2[0] and r1[1] <= r2[1]
+
+
+def has_overlap(r1, r2):
+    r1, r2 = map(set, map(lambda r: range(r[0], r[1] + 1), [r1, r2]))
+    return r1.intersection(r2)

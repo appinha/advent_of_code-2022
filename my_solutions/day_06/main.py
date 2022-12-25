7619cd95081406e6e1eec6570a595e9bdc1f7ce6
appinha <amanda_pinha@hotmail.com>
2022-12-25 19:02:02 -0300

    [day_06] Some minor revisions

--- my_solutions/day_06/main.py
+++ my_solutions/day_06/main.py
@@ -8,13 +8,14 @@ class DayPuzzleSolver():
         self.delimiter = ""
 
     def solve_part_1(self, raw_input):
-        return self._find_marker(raw_input, 4)
+        return find_marker(raw_input, 4)
 
     def solve_part_2(self, raw_input):
-        return self._find_marker(raw_input, 14)
+        return find_marker(raw_input, 14)
 
-    def _find_marker(self, string, n):
-        for i in range(len(string) - (n - 1)):
-            substring = string[i:i + n]
-            if len(set(substring)) == n:
-                return i + n
+
+def find_marker(string, n):
+    for i in range(len(string) - (n - 1)):
+        substring = string[i:i + n]
+        if len(set(substring)) == n:
+            return i + n

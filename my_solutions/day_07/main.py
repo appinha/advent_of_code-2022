cd1cdaa31f48c63c45a41ed66de70a234637c38b
appinha <amanda_pinha@hotmail.com>
2022-12-25 19:10:48 -0300

    [day_07] Some minor revisions

--- my_solutions/day_07/main.py
+++ my_solutions/day_07/main.py
@@ -10,46 +10,48 @@ class DayPuzzleSolver():
         self.delimiter = "\n"
 
     def solve_part_1(self, raw_input):
-        size_by_path = self._get_dir_sizes(raw_input)
-        return sum(size for size in size_by_path.values() if size <= 100000)
+        dir_size_by_path = process_input(raw_input)
+        return sum(size for size in dir_size_by_path.values() if size <= 100000)
 
     def solve_part_2(self, raw_input):
-        size_by_path = self._get_dir_sizes(raw_input)
-        needed_space = self._calculate_needed_space(size_by_path["/"])
-        return min(size for size in size_by_path.values() if size > needed_space)
+        dir_size_by_path = process_input(raw_input)
+        needed_space = calculate_needed_space(dir_size_by_path["/"])
+        return min(size for size in dir_size_by_path.values() if size > needed_space)
 
-    def _get_dir_sizes(self, raw_input):
-        path_stack = lib.Stack()
-        contents_by_path = defaultdict(list)
-        size_by_path = defaultdict(int)
 
-        def build_curr_path():
-            return "/".join(path_stack.items)
+def process_input(raw_input):
+    path_stack = lib.Stack()
+    contents_by_path = defaultdict(list)
+    size_by_path = defaultdict(int)
 
-        def sum_contents_then_pop(curr_path):
-            size_by_path[curr_path] = sum(content if lib.is_int(content) else size_by_path[content]
-                for content in contents_by_path[curr_path])
-            path_stack.pop()
+    def build_curr_path():
+        return "/".join(path_stack.items)
 
-        for line in raw_input:
-            curr_path = build_curr_path()
-            match line.strip("$").split():
-                case ["cd", ".."]:
-                    sum_contents_then_pop(curr_path)
-                case ["cd", dir_name]:
-                    path_stack.push(dir_name)
-                case ["dir", dir_name]:
-                    contents_by_path[curr_path].append(curr_path + "/" + dir_name)
-                case [size, _]:
-                    contents_by_path[curr_path].append(int(size))
+    def sum_contents_then_pop(curr_path):
+        size_by_path[curr_path] = sum(content if lib.is_int(content) else size_by_path[content]
+            for content in contents_by_path[curr_path])
+        path_stack.pop()
 
-        while path_stack.items:
-            sum_contents_then_pop(build_curr_path())
+    for line in raw_input:
+        curr_path = build_curr_path()
+        match line.strip("$").split():
+            case ["cd", ".."]:
+                sum_contents_then_pop(curr_path)
+            case ["cd", dir_name]:
+                path_stack.push(dir_name)
+            case ["dir", dir_name]:
+                contents_by_path[curr_path].append(curr_path + "/" + dir_name)
+            case [size, _]:
+                contents_by_path[curr_path].append(int(size))
 
-        return size_by_path
+    while path_stack.items:
+        sum_contents_then_pop(build_curr_path())
 
-    def _calculate_needed_space(self, used_space):
-        AVAILABLE_SPACE = 70_000_000
-        UPDATE_SIZE = 30_000_000
-        unused_space = AVAILABLE_SPACE - used_space
-        return UPDATE_SIZE - unused_space
+    return size_by_path
+
+
+def calculate_needed_space(used_space):
+    AVAILABLE_SPACE = 70_000_000
+    UPDATE_SIZE = 30_000_000
+    unused_space = AVAILABLE_SPACE - used_space
+    return UPDATE_SIZE - unused_space

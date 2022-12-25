568111790841d4379fc5e8258e8d04efb02b7567
appinha <amanda_pinha@hotmail.com>
2022-12-25 18:52:30 -0300

    [day_02] Some minor revisions

--- my_solutions/day_02/main.py
+++ my_solutions/day_02/main.py
@@ -9,7 +9,7 @@ class DayPuzzleSolver():
 
     def solve_part_1(self, raw_input):
         # Rock == A == X: 1; Paper == B == Y: 2; Scissors == C == Z: 3
-        score_for_shape = {"X": 1, "Y": 2, "Z": 3}
+        score_by_shape = {"X": 1, "Y": 2, "Z": 3}
         score_by_you_by_opponent = {
             "A": { "Y": 6, "X": 3, "Z": 0},
             "B": { "Z": 6, "Y": 3, "X": 0},
@@ -19,13 +19,13 @@ class DayPuzzleSolver():
         total_score = 0
         for round in raw_input:
             opp, you = round.split(" ")
-            total_score += score_for_shape[you] + score_by_you_by_opponent[opp][you]
+            total_score += score_by_shape[you] + score_by_you_by_opponent[opp][you]
         return total_score
 
     def solve_part_2(self, raw_input):
         # Rock == A: 1; Paper == B: 2; Scissors == C: 3
         # X == lose, Y == draw, Z == win
-        score_for_shape = {"A": 1, "B": 2, "C": 3}
+        score_by_shape = {"A": 1, "B": 2, "C": 3}
         score_by_end = {"X": 0, "Y": 3, "Z": 6}
         shape_by_end_by_opponent = {
             "A": { "X": "C", "Y": "A", "Z": "B"},
@@ -37,5 +37,5 @@ class DayPuzzleSolver():
         for round in raw_input:
             opp, end = round.split(" ")
             your_shape = shape_by_end_by_opponent[opp][end]
-            total_score += score_for_shape[your_shape] + score_by_end[end]
+            total_score += score_by_shape[your_shape] + score_by_end[end]
         return total_score

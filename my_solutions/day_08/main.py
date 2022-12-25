8b92f78b1d5d1ac0fed92173eb2ecb8c52be98a0
appinha <amanda_pinha@hotmail.com>
2022-12-25 19:15:46 -0300

    [day_08] Some minor revisions

--- my_solutions/day_08/main.py
+++ my_solutions/day_08/main.py
@@ -8,33 +8,34 @@ class DayPuzzleSolver():
         self.delimiter = ""
 
     def solve_part_1(self, raw_input):
-        trees = lib.Grid(string=raw_input, type=int)
+        trees = lib.Grid(string=raw_input, dtype=int)
 
-        def is_not_visible_in_line(pos, line):
-            return any(trees[pos] <= trees[lp] for lp in line)
+        def is_not_visible_in_line(location, line):
+            return any(trees[location] <= trees[lp] for lp in line)
 
         not_visible_trees = []
-        for pos, lines in trees.orthogonal_adjacencies_by_pos.items():
+        for location, lines in trees.orthogonal_neighbours_by_location.items():
             if any([not line for line in lines]):
                 continue
-            if all(is_not_visible_in_line(pos, line) for line in lines):
-                not_visible_trees.append(pos)
+            if all(is_not_visible_in_line(location, line) for line in lines):
+                not_visible_trees.append(location)
 
         return trees.len - len(not_visible_trees)
 
     def solve_part_2(self, raw_input):
-        trees = lib.Grid(string=raw_input, type=int)
+        trees = lib.Grid(string=raw_input, dtype=int)
 
         highest_score = 0
-        for pos, lines in trees.orthogonal_adjacencies_by_pos.items():
+        for location, lines in trees.orthogonal_neighbours_by_location.items():
             scores = 1
             for line in lines:
                 score = 0
                 for lp in line:
                     score += 1
-                    if trees[pos] <= trees[lp]:
+                    if trees[location] <= trees[lp]:
                         break
                 scores *= score
             if scores > highest_score:
                 highest_score = scores
+
         return highest_score

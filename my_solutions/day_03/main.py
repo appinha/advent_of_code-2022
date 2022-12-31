a31dc99dc7f423b1ce655901a65fb03a41f71cfb
appinha <amanda_pinha@hotmail.com>
2022-12-31 13:41:33 -0300

    [day_03] Some minor improvements

--- my_solutions/day_03/main.py
+++ my_solutions/day_03/main.py
@@ -10,21 +10,20 @@ from string import ascii_letters
 class DayPuzzleSolver():
     def __init__(self):
         self.delimiter = "\n"
-        self.priority = ascii_letters
 
-    def solve_part_1(self, raw_input):
+    def solve_part_1(self, raw_input: str):
         total_priorities = 0
         for rucksack in raw_input:
             compartment_size = len(rucksack) // 2
-            comp_1, comp_2 = rucksack[:compartment_size], rucksack[compartment_size:]
-            common = set(comp_1).intersection(comp_2).pop()
-            total_priorities += self.priority.index(common) + 1
+            compartment_1, compartment_2 = rucksack[:compartment_size], rucksack[compartment_size:]
+            common_item = set(compartment_1).intersection(compartment_2).pop()
+            total_priorities += ascii_letters.index(common_item) + 1
         return total_priorities
 
-    def solve_part_2(self, raw_input):
+    def solve_part_2(self, raw_input: str):
         groups = [raw_input[i:i + 3] for i in range(0, len(raw_input), 3)]
         total_priorities = 0
         for group in groups:
-            common = set(group[0]).intersection(group[1]).intersection(group[2]).pop()
-            total_priorities += self.priority.index(common) + 1
+            common_item = set(group[0]).intersection(group[1]).intersection(group[2]).pop()
+            total_priorities += ascii_letters.index(common_item) + 1
         return total_priorities

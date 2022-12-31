7fc406391fa89dead741a44b65e7a310d5646d9d
appinha <amanda_pinha@hotmail.com>
2022-12-31 10:54:07 -0300

    [day_01] Adds type hints

--- my_solutions/day_01/main.py
+++ my_solutions/day_01/main.py
@@ -9,14 +9,14 @@ class DayPuzzleSolver():
     def __init__(self):
         self.delimiter = "\n\n"
 
-    def solve_part_1(self, raw_input):
+    def solve_part_1(self, raw_input: str):
         calories_totals = process_input(raw_input)
         return heapq.nlargest(1, calories_totals)[0]
 
-    def solve_part_2(self, raw_input):
+    def solve_part_2(self, raw_input: str):
         calories_totals = process_input(raw_input)
         return sum(heapq.nlargest(3, calories_totals))
 
 
-def process_input(raw_input):
+def process_input(raw_input: str):
     return [sum(map(int, elf.split("\n"))) for elf in raw_input]

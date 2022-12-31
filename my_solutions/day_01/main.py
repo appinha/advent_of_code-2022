0eba32b7d5277d4b2a05159a5859024202558303
appinha <amanda_pinha@hotmail.com>
2022-12-31 17:33:33 -0300

    [day_01] Minor revision

--- my_solutions/day_01/main.py
+++ my_solutions/day_01/main.py
@@ -11,7 +11,7 @@ class DayPuzzleSolver():
 
     def solve_part_1(self, raw_input: str):
         calories_totals = process_input(raw_input)
-        return heapq.nlargest(1, calories_totals)[0]
+        return max(calories_totals)
 
     def solve_part_2(self, raw_input: str):
         calories_totals = process_input(raw_input)

a3c4be9128725b4e97859598f0e8e95deb043c48
appinha <amanda_pinha@hotmail.com>
2022-12-31 13:55:19 -0300

    [day_06] Adds type hints

--- my_solutions/day_06/main.py
+++ my_solutions/day_06/main.py
@@ -9,14 +9,14 @@ class DayPuzzleSolver():
     def __init__(self):
         self.delimiter = ""
 
-    def solve_part_1(self, raw_input):
+    def solve_part_1(self, raw_input: str):
         return find_marker(raw_input, 4)
 
-    def solve_part_2(self, raw_input):
+    def solve_part_2(self, raw_input: str):
         return find_marker(raw_input, 14)
 
 
-def find_marker(string, n):
+def find_marker(string: str, n: int):
     for i in range(len(string) - (n - 1)):
         substring = string[i:i + n]
         if len(set(substring)) == n:

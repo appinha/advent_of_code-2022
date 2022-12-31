3949c9e6c688f125530d7fca960b0303b6906858
appinha <amanda_pinha@hotmail.com>
2022-12-31 11:17:21 -0300

    [day_02] Some minor improvements

--- my_solutions/day_02/main.py
+++ my_solutions/day_02/main.py
@@ -9,35 +9,35 @@ class DayPuzzleSolver():
     def __init__(self):
         self.delimiter = "\n"
 
-    def solve_part_1(self, raw_input):
+    def solve_part_1(self, raw_input: str):
+        rounds = process_input(raw_input)
+
         # Rock == A == X: 1; Paper == B == Y: 2; Scissors == C == Z: 3
-        score_by_shape = {"X": 1, "Y": 2, "Z": 3}
-        score_by_you_by_opponent = {
+        SCORE_BY_SHAPE = {"X": 1, "Y": 2, "Z": 3}
+        SCORE_BY_YOU_BY_OPPONENT = {
             "A": { "Y": 6, "X": 3, "Z": 0},
             "B": { "Z": 6, "Y": 3, "X": 0},
             "C": { "X": 6, "Z": 3, "Y": 0},
         }
 
-        total_score = 0
-        for round in raw_input:
-            opp, you = round.split(" ")
-            total_score += score_by_shape[you] + score_by_you_by_opponent[opp][you]
-        return total_score
+        return sum(SCORE_BY_SHAPE[you] + SCORE_BY_YOU_BY_OPPONENT[opp][you] for opp, you in rounds)
 
-    def solve_part_2(self, raw_input):
+    def solve_part_2(self, raw_input: str):
+        rounds = process_input(raw_input)
+
         # Rock == A: 1; Paper == B: 2; Scissors == C: 3
         # X == lose, Y == draw, Z == win
-        score_by_shape = {"A": 1, "B": 2, "C": 3}
-        score_by_end = {"X": 0, "Y": 3, "Z": 6}
-        shape_by_end_by_opponent = {
+        SCORE_BY_SHAPE = {"A": 1, "B": 2, "C": 3}
+        SCORE_BY_END = {"X": 0, "Y": 3, "Z": 6}
+        SHAPE_BY_END_BY_OPPONENT = {
             "A": { "X": "C", "Y": "A", "Z": "B"},
             "B": { "X": "A", "Y": "B", "Z": "C"},
             "C": { "X": "B", "Y": "C", "Z": "A"},
         }
 
-        total_score = 0
-        for round in raw_input:
-            opp, end = round.split(" ")
-            your_shape = shape_by_end_by_opponent[opp][end]
-            total_score += score_by_shape[your_shape] + score_by_end[end]
-        return total_score
+        return sum(SCORE_BY_SHAPE[SHAPE_BY_END_BY_OPPONENT[opp][end]] + SCORE_BY_END[end]
+            for opp, end in rounds)
+
+
+def process_input(raw_input: str):
+    return [line.split(" ") for line in raw_input]
